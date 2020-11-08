6d78cabbce98be8859ee540ae974aeffcd31536a
theProgrammerDavid <davidvelho2awesome@gmail.com>
2020-11-09 02:23:44 +0530

    fix: include errors on Windows

--- include/YamlUtil.hpp
+++ include/YamlUtil.hpp
@@ -5,7 +5,7 @@
 #include <string>
 #include <vector>
 #include <algorithm>
-
+#include <functional>
 #include <string.h>
 
 // #include <functional>
--- include/main.hpp
+++ include/main.hpp
@@ -1,2 +1,2 @@
-#include <bits/stdc++.h>
+
 #include "YamlParser.hpp"
