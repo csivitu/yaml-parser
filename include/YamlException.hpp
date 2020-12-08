b7219c7d6ec10f2243fa3b1a480abaad1b50d11b
theProgrammerDavid <davidvelho2awesome@gmail.com>
2020-12-08 23:49:32 +0530

    refractor: rewrite YamlException, remove extra exception types

--- include/YamlException.hpp
+++ include/YamlException.hpp
@@ -1,51 +1,43 @@
 #pragma once
 
 #include <iostream>
+#include <exception>
 
 namespace YamlParser
 {
-    class InputSubSystemException : public std::exception
-    {
-    public:
-        const char *what() const noexcept override
-        {
-            return "Provide more details to the exception";
-        }
-    };
 
-    class YamlNodeScalarChildError : public std::exception
-    {
-    public:
-        const char *what() const noexcept override
-        {
-            return "Type SCALAR Cannot have Children";
-        }
-    };
+    class YamlException: virtual public std::exception {
+    
+protected:
 
-    class ScalarNodeAssignmentError : public std::exception
-    {
-    public:
-        const char *what() const noexcept override
-        {
-            return "Type SCALAR Cannot have Children";
-        }
-    };
+  
+    std::string error_message;      ///< Error message
+    
+public:
 
-    class VectorNodeAssignmentError : public std::exception
-    {
-    public:
-        const char *what() const noexcept override
-        {
-            return "Type VECTOR Cannot be assigned";
-        }
-    };
-    class MapNodeAssignmentError : public std::exception
-    {
-    public:
-        const char *what() const noexcept override
-        {
-            return "Type MAP Cannot be assigned";
-        }
-    };
+    /** Constructor (C++ STL string, int, int).
+     *  @param msg The error message
+    
+     */
+    explicit 
+    YamlException(const std::string& msg):
+       error_message(msg)
+        {}
+
+    /** Destructor.
+     *  Virtual to allow for subclassing.
+     */
+    virtual ~YamlException() throw () {}
+
+    /** Returns a pointer to the (constant) error description.
+     *  @return A pointer to a const char*. The underlying memory
+     *  is in possession of the YamlException object. Callers must
+     *  not attempt to free the memory.
+     */
+    virtual const char* what() const throw () {
+       return error_message.c_str();
+    }
+
+};
 
 } // namespace YamlParser
